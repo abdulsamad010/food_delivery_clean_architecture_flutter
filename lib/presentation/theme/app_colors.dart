f9012bfabfd85dc82dcfcde4182bb9abadc3384a
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-09-22 23:32:37 +0500

    added app colors

---
+++ lib/presentation/theme/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+
+const Color orange = Color(0xFFFF6B00);
+const Color darkOrange = Color(0xFFE85D04);
+const Color lightOrange = Color(0xFFFFE5D0);
+
+const Color lightGrey = Color(0xFFF5F5F5);
+const Color grey = Color(0xFFBDBDBD);
+const Color darkGrey = Color(0xFF757575);
+
+const Color blueGrey = Color(0xFF607D8B);
+const Color lightBlueGrey = Color(0xFFD7E5E8);
+
+const Color yellow = Color(0xFFFFC107);
+const Color golden = Color(0xFFFFB300);
+
+const Color darkBlue = Color(0xFF1A1329);
