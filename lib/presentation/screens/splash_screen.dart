76b7ff464a43dfea586fa3795581ef50becdebd2
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-09-23 05:59:25 +0500

    implemented splash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,6 +14,27 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Expanded(child: SizedBox()),
+
+            SvgPicture.asset("assets/icons/Logo.svg"),
+
+            Expanded(child: SizedBox()),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SvgPicture.asset("assets/icons/Ellipse 1006.svg"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
